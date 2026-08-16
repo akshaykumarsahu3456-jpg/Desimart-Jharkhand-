6ec5e6cb6cf37d9bfac12e265245e8a25727cf1e
akshaykumarsahu3456-jpg <akshaykumarsahu3456@gmail.com>
2026-08-16 15:16:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
 constt express = require("express");
 const cors = require("cors");
 const app = express();
